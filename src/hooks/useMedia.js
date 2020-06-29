ffb722a2b7f6f98a4d5ffadcdaba5e8878ab3fbf
Daren Malfait <daren@wheelhouse.be>
2020-06-29 21:31:52 +0200

    fix window is not defined error

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 
+const isBrowser = typeof window !== 'undefined';
+
 export const useMedia = (queries, values, defaultValue) => {
   // Array containing a media query list for each query
-  const mediaQueryLists = queries.map((q) => window.matchMedia(q));
+  const mediaQueryLists = queries.map((q) => isBrowser && window.matchMedia(q));
 
   // Function that gets value based on matching media query
   const getValue = () => {

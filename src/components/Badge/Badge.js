c6742e6f14110c42531ca998b0693b392a6fe392
Daren Malfait <daren@wheelhouse.be>
2020-06-29 15:23:30 +0200

    add badge styling

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -3,14 +3,23 @@ import PropTypes from 'prop-types';
 
 import { BadgeContainer } from './badge.styles';
 
-const Badge = ({ children }) => (
-  <BadgeContainer>
+import colors from '../../styles/colors';
+
+const Badge = ({ background, children, color }) => (
+  <BadgeContainer background={background} color={color}>
     {children}
   </BadgeContainer>
 );
 
 Badge.propTypes = {
+  background: PropTypes.string,
   children: PropTypes.string.isRequired,
+  color: PropTypes.string,
+};
+
+Badge.defaultProps = {
+  background: colors.accent,
+  color: colors.textLight,
 };
 
 export default memo(Badge);

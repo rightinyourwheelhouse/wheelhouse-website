78d8fd199d23c1f02cdb119a85eff44cabe61b45
Daren Malfait <daren@wheelhouse.be>
2020-07-08 08:55:38 +0200

    split teamrotation into seperate api call

---
+++ src/api/images/useTeamRotationImages.js
@@ -0,0 +1,37 @@
+import { useMemo } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+
+const query = graphql`
+  query {
+    allFile(
+      sort: { fields: name, order: DESC }
+      filter: { relativeDirectory: { eq: "team-rotation" } }
+    ) {
+      edges {
+        node {
+          id
+          name
+          childImageSharp {
+            fluid(maxHeight: 300) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const useTeamRotationImages = () => {
+  const {
+    allFile: { edges },
+  } = useStaticQuery(query);
+
+  const images = useMemo(() => {
+    const nodes = edges.map(({ node }) => ({ ...node }));
+
+    return nodes;
+  }, [edges]);
+
+  return images;
+};

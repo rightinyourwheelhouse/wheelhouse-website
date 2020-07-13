a775efa851185317930da80285eb918e4d9731fb
Daren Malfait <daren@wheelhouse.be>
2020-07-13 12:50:43 +0200

    add minor styling changes

--- src/components/Onboarding/_stage.styles.js
+++ src/components/Onboarding/_stage.styles.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
+import spacing from '~styles/spacing';
+
 export const StageContainer = styled.div`
   height: 270px;
   margin-bottom: 40px;
   opacity: ${({ visible }) => (visible ? 1 : 0)};
-
   transition: opacity 0.2s ease-in-out;
+
+  > div {
+    margin-bottom: ${spacing.default};
+  }
 `;

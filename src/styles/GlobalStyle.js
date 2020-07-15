b3a293854638fee82befb412fb9d52f32259221f
Daren Malfait <daren@wheelhouse.be>
2020-07-15 14:57:07 +0200

    return the caps in titles

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -94,13 +94,13 @@ export const GlobalStyle = createGlobalStyle`
     font-family: ${fonts.family};
     margin-top: 1.5rem;
     margin-bottom: 0;
-    line-height: 1.1;
-    text-transform: lowercase;
+    line-height: 1.5rem;
+    text-transform: uppercase;
   }
 
   h1 {
     font-size: 3.242rem;
-    line-height: 1.1;
+    line-height: 0.9;
     font-weight: 900;
     margin-top: 0;
 
@@ -120,7 +120,7 @@ export const GlobalStyle = createGlobalStyle`
 
   h2 {
     font-size: 3rem;
-    line-height: 1.1;
+    line-height: 0.9;
     font-weight: 900;
     margin-top: 0;
     margin-bottom: 1rem;

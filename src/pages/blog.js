826638c46e180baa1ea5a2aa7dc4538b156e631a
Daren Malfait <daren@wheelhouse.be>
2020-07-15 18:17:06 +0200

    add blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { Section, Container } from '~components/layoutComponents';
+import Content from '~components/Content';
+import SEO from '~components/SEO';
+import SubTitle from '~components/SubTitle';
+
+import Layout from '~layouts/default';
+
+import BlogOverview from '~modules/BlogOverview';
+import Navigation from '~modules/Navigation';
+
+const TeamPage = () => (
+  <Layout>
+    <SEO title="Blogposts about javascript in wheelhouse" />
+    <Navigation />
+
+    <Section>
+      <Container>
+        <Content>
+          <SubTitle>Blog</SubTitle>
+          <h2>Writing talent</h2>
+          <p>
+            Some text about blogposts and why we like them
+          </p>
+          <BlogOverview />
+        </Content>
+      </Container>
+    </Section>
+  </Layout>
+);
+
+export default TeamPage;

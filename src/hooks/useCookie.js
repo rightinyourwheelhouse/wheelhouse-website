18156c2a19c03650c7d64f1f21a55a72a314e373
Daren Malfait <daren@wheelhouse.be>
2020-09-02 18:52:38 +0200

    fix gatsby building errors

--- src/hooks/useCookie.js
+++ src/hooks/useCookie.js
@@ -6,18 +6,26 @@ export const setCookie = (name, value, options) => {
     path: '/',
     ...options,
   };
+
   const expires = new Date(
     Date.now() + optionsWithDefaults.days * 864e5
   ).toUTCString();
-  document.cookie = `${name}=${encodeURIComponent(
-    value
-  )}; expires=${expires}; path=${optionsWithDefaults.path}`;
+
+  if (typeof document !== undefined) {
+    document.cookie = `${name}=${encodeURIComponent(
+      value
+    )}; expires=${expires}; path=${optionsWithDefaults.path}`;
+  }
 };
 
-export const getCookie = (name) => document.cookie.split('; ').reduce((r, v) => {
-  const parts = v.split('=');
-  return parts[0] === name ? decodeURIComponent(parts[1]) : r;
-}, '');
+export const getCookie = (name) => {
+  if (typeof document !== undefined) {
+    document.cookie.split('; ').reduce((r, v) => {
+      const parts = v.split('=');
+      return parts[0] === name ? decodeURIComponent(parts[1]) : r;
+    }, '');
+  }
+};
 
 export const useCookie = (key, initialValue) => {
   const [item, setItem] = useState(() => getCookie(key) || initialValue);

bfefc4a080e2be40d614bc29eec353323ff9d829
Daren Malfait <daren@wheelhouse.be>
2020-12-04 11:42:32 +0100

    fix twocolumn layout

--- src/components/TwoColumns/TwoColumns.js
+++ src/components/TwoColumns/TwoColumns.js
@@ -1,24 +1,26 @@
 import styled from 'styled-components';
 
+import breakpoints from '~styles/breakpoints';
+
 export default styled.div`
   --direction: ${({ reversed }) => (reversed ? 'row-reverse' : 'row')};
   --first-column-width: ${({ firstColumnWidth }) => firstColumnWidth || '45%'};
 
-  display: flex;
-  flex-wrap: wrap;
-  margin: calc(var(--spacing-large) / 2 * -1);
-  flex-direction: var(--direction);
+  @media screen and (min-width: ${breakpoints.medium}) {
+    display: flex;
+    flex-wrap: wrap;
+    margin: calc(var(--spacing-large) / 2 * -1);
+    flex-direction: var(--direction);
 
-  > * {
-    margin: calc(var(--spacing-large) / 2);
-    flex-basis: var(--first-column-width);
-    flex-grow: 1;
+    > * {
+      margin: calc(var(--spacing-large) / 2);
+      flex-basis: var(--first-column-width);
+      flex-grow: 1;
 
-
-    &:last-child {
-      flex-basis: 0;
-      flex-grow: 999;
-      min-width: calc(60% - var(--spacing-large));
+      &:last-child {
+        flex-basis: 0;
+        flex-grow: 999;
+      }
     }
   }
 `;

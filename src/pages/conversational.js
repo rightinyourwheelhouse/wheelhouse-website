288474fb4d3c4524ca1deeb3ace28cef8ed1f56b
jaspervermeulen <jaspervermeulen@icloud.com>
2021-10-20 18:42:44 +0200

    feat: add conversional page

---
+++ src/pages/conversational.js
@@ -0,0 +1,57 @@
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { Section } from '~components/layoutComponents';
+import Seo from '~components/SEO';
+import Layout from '~layouts/default';
+import Navigation from '~modules/Navigation';
+
+function ConversationalPage({
+  data: {
+    pagesJson: { seo },
+  },
+}) {
+  return (
+    <Layout>
+      <Seo title={seo.title} description={seo.description} image={seo.image} />
+      <Navigation />
+      <Section>
+        <p>Hello World!</p>
+      </Section>
+    </Layout>
+  );
+}
+
+ConversationalPage.propTypes = {
+  data: PropTypes.shape({
+    pagesJson: PropTypes.shape({
+      seo: PropTypes.shape({
+        article: PropTypes.bool,
+        description: PropTypes.string,
+        image: PropTypes.shape({
+          childImageSharp: PropTypes.shape({
+            resize: PropTypes.shape({
+              src: PropTypes.string,
+            }),
+          }),
+        }),
+        lang: PropTypes.string,
+        meta: PropTypes.arrayOf(PropTypes.object),
+        title: PropTypes.string,
+      }),
+    }),
+  }).isRequired,
+};
+
+export default ConversationalPage;
+
+export const privacyPolicyQuery = graphql`
+  query ConversationalPage {
+    pagesJson(page: { eq: "conversational" }) {
+      seo {
+        ...seo
+      }
+    }
+  }
+`;

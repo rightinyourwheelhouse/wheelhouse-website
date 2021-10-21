cb37292fd05036566785a6b701b33e86fd02c36d
Daren Malfait <daren@wheelhouse.be>
2021-10-21 08:37:44 +0200

    fix(job-overview): hide when no jobs

--- src/modules/JobOverview/JobOverview.js
+++ src/modules/JobOverview/JobOverview.js
@@ -1,6 +1,6 @@
 import Link from 'gatsby-link';
 import PropTypes from 'prop-types';
-import React, { memo } from 'react';
+import * as React from 'react';
 
 import {
   JobItemContainer,
@@ -9,17 +9,24 @@ import {
 } from './jobOverview.styles';
 
 import CircleBackground from '~components/CircleBackground';
-
 import Stack from '~components/Stack';
+import SubTitle from '~components/SubTitle';
 
 import Arrow from '~images/icons/arrow-top-right.svg';
 import { useJobOverview } from '~services/job/useJobOverview';
 
-const JobOverview = ({ current }) => {
+function JobOverview({ current, title, subTitle }) {
   const jobs = useJobOverview({ count: 2, current });
+
+  if (!jobs || jobs.length === 0) {
+    return null;
+  }
 
   return (
     <div>
+      {subTitle && <SubTitle>{subTitle}</SubTitle>}
+      {title && <h2>{title}</h2>}
+
       <Stack space="60px">
         <div>
           {jobs.map(({ title, slug, city }) => (
@@ -39,14 +46,16 @@ const JobOverview = ({ current }) => {
       </Stack>
     </div>
   );
-};
+}
 
 JobOverview.propTypes = {
   current: PropTypes.string,
+  title: PropTypes.string,
+  subTitle: PropTypes.string,
 };
 
 JobOverview.defaultProps = {
   current: null,
 };
 
-export default memo(JobOverview);
+export default JobOverview;

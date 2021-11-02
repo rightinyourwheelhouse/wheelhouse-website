493a11730f925b816ec2fd638fa66cb9631d9c03
wotusay <wout.salembier@hotmail.com>
2021-11-02 11:23:13 +0100

    refactor: removed all arrow functions on modules

--- src/modules/RadioRaccoons/RadioRaccoons.js
+++ src/modules/RadioRaccoons/RadioRaccoons.js
@@ -7,27 +7,29 @@ import Button from '~components/Button';
 import RadioRaccoonsLogo from '~images/radio-raccoons.svg';
 import colors from '~styles/colors';
 
-const RadioRaccoons = () => (
-  <OuterContainer>
-    <RadioRaccoonsLogo />
-    <p>Radio Raccoons is a podcast from our very own business group.</p>
-    <p>
-      Each episode contains an expert answering questions about the most
-      important new technologies.
-    </p>
-    <Button
-      background={colors.textPrimary100}
-      color={colors.textPrimary900}
-      backgroundHover={colors.textPrimary100}
-      colorHover={colors.textPrimary900}
-      as="a"
-      href="https://radio.raccoons.be/"
-      rel="nofollow noreferrer"
-      target="_blank"
-    >
-      Listen to the podcast
-    </Button>
-  </OuterContainer>
-);
+function RadioRaccoons() {
+  return (
+    <OuterContainer>
+      <RadioRaccoonsLogo />
+      <p>Radio Raccoons is a podcast from our very own business group.</p>
+      <p>
+        Each episode contains an expert answering questions about the most
+        important new technologies.
+      </p>
+      <Button
+        background={colors.textPrimary100}
+        color={colors.textPrimary900}
+        backgroundHover={colors.textPrimary100}
+        colorHover={colors.textPrimary900}
+        as="a"
+        href="https://radio.raccoons.be/"
+        rel="nofollow noreferrer"
+        target="_blank"
+      >
+        Listen to the podcast
+      </Button>
+    </OuterContainer>
+  );
+}
 
 export default memo(RadioRaccoons);

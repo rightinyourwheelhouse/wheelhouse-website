103ace9af2f9bcfc460600a80edd063c7d0a3124
wotusay <wout.salembier@hotmail.com>
2021-11-02 10:32:21 +0100

    refactor: changed opensource component for megre conflict

--- src/modules/OpenSource/OpenSource.js
+++ src/modules/OpenSource/OpenSource.js
@@ -7,30 +7,27 @@ import Button from '~components/Button';
 
 import Github from '~images/github.svg';
 
-function OpenSource() {
-  return (
-    <OpenSourceContainer>
-      <h2>Have you seen this cat?</h2>
-      <Github />
-      <p>
-        We decided to make our website source code public. We also try to make
-        our entire team use it as a playground and contribute to make the
-        website reflect who we really are.
-      </p>
-      <Button
-        as="a"
-        href="https://github.com/rightinyourwheelhouse/wheelhouse-website"
-        target="_blank"
-        rel="noreferrer nofollow"
-      >
-        Check it out
-      </Button>
-      <p>
-        Or check out the{' '}
-        <Link to="/projects"> projects from our wheelies </Link>
-      </p>
-    </OpenSourceContainer>
-  );
-}
+const OpenSource = () => (
+  <OpenSourceContainer>
+    <h2>Have you seen this cat?</h2>
+    <Github />
+    <p>
+      We decided to make our website source code public. We also try to make our
+      entire team use it as a playground and contribute to make the website
+      reflect who we really are.
+    </p>
+    <Button
+      as="a"
+      href="https://github.com/rightinyourwheelhouse/wheelhouse-website"
+      target="_blank"
+      rel="noreferrer nofollow"
+    >
+      Check it out
+    </Button>
+    <p>
+      Or check out the <Link to="/projects"> projects from our wheelies </Link>
+    </p>
+  </OpenSourceContainer>
+);
 
 export default memo(OpenSource);

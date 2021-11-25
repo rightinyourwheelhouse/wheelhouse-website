73dc4a1264648e1ab26166e853d045e2032dfbfa
jaspervermeulen <jaspervermeulen@icloud.com>
2021-11-25 15:00:15 +0100

    chore: add conversational overview component

---
+++ src/components/ConversationalOverview/ConversationalOverview.js
@@ -0,0 +1,107 @@
+import { ArrowDownIcon } from '@heroicons/react/solid';
+import React, { useState } from 'react';
+
+import * as S from './conversationalOverview.styles';
+
+function ConversationalOverview({
+  name,
+  expectations,
+  education,
+  experience,
+  hobbies,
+  cv,
+  insight,
+  phone,
+  mail,
+  portfolioLink1,
+  portfolioLink2,
+  portfolioLink3,
+}) {
+  const [overview, setOverwiew] = useState(false);
+
+  return (
+    <>
+      <S.OverviewButton
+        onClick={() => {
+          setOverwiew(!overview);
+        }}
+        type="button"
+      >
+        <span style={{ fontSize: '18px', fontFamily: 'Montserrat' }}>
+          See my anwsers
+        </span>
+        <div
+          style={
+            overview
+              ? {
+                  transform: 'rotate(180deg)',
+                }
+              : {
+                  transform: 'rotate(0deg)',
+                }
+          }
+        >
+          <ArrowDownIcon width="24" />
+        </div>
+      </S.OverviewButton>
+      <div
+        style={
+          overview
+            ? {
+                display: 'inline',
+                backgroundColor: 'gray',
+              }
+            : {
+                display: 'none',
+                backgroundColor: 'gray',
+              }
+        }
+      >
+        <S.OverviewWrapper>
+          <div>
+            <S.OverviewLabel>Name:</S.OverviewLabel>
+            <S.OverviewText>{name}</S.OverviewText>
+          </div>
+          <div>
+            <S.OverviewLabel>Expectations:</S.OverviewLabel>
+            <S.OverviewText>{expectations}</S.OverviewText>
+          </div>
+          <div>
+            <S.OverviewLabel>Education:</S.OverviewLabel>
+            <S.OverviewText>{education}</S.OverviewText>
+          </div>
+          <div>
+            <S.OverviewLabel>Experience:</S.OverviewLabel>
+            <S.OverviewText>{experience}</S.OverviewText>
+          </div>
+          <div>
+            <S.OverviewLabel>Hobbies:</S.OverviewLabel>
+            <S.OverviewText>{hobbies}</S.OverviewText>
+          </div>
+          <div>
+            <S.OverviewLabel>Cv:</S.OverviewLabel>
+            <S.OverviewText>{cv}</S.OverviewText>
+          </div>
+          <div>
+            <S.OverviewLabel>Insight:</S.OverviewLabel>
+            <S.OverviewText>{insight}</S.OverviewText>
+          </div>
+          <div>
+            <S.OverviewLabel>Contact:</S.OverviewLabel>
+            <S.OverviewText>{phone}</S.OverviewText>
+            <S.OverviewText>{mail}</S.OverviewText>
+          </div>
+
+          <div>
+            <S.OverviewLabel>Portfolio:</S.OverviewLabel>
+            <S.OverviewText>{portfolioLink1 || ''}</S.OverviewText>
+            <S.OverviewText>{portfolioLink2 || ''}</S.OverviewText>
+            <S.OverviewText>{portfolioLink3 || ''}</S.OverviewText>
+          </div>
+        </S.OverviewWrapper>
+      </div>
+    </>
+  );
+}
+
+export default ConversationalOverview;
